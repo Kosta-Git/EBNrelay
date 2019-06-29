4b80be01e3599e4799750450053b0db2b830a454
Kosta <k.sovaridis@gmail.com>
2019-06-30 00:10:54 +0200

    Wrap up commit

--- src/data-types/Realm.ts
+++ src/data-types/Realm.ts
@@ -6,10 +6,12 @@ export class Realm {
     public pos: WorldPosData;
     public players: Number;
     public name: String;
+    public id : number
 
-    constructor( worldPos: WorldPosData, players: Number, name: String ) {
+    constructor( worldPos: WorldPosData, players: Number, name: String, id: number ) {
         this.pos = worldPos;
         this.players = players;
         this.name = name;
+        this.id = id;
     }
 }
--- src/eventbot-plugin.ts
+++ src/eventbot-plugin.ts
@@ -1,4 +1,4 @@
-import { Client, ObjectData, UpdatePacket, NewTickPacket, Library, PacketHook, PlayerTextPacket, TextPacket, WorldPosData, Logger, RealmHeroesLeftPacket } from 'nrelay';
+import { Client, ObjectData, UpdatePacket, NewTickPacket, Library, PacketHook, PlayerTextPacket, TextPacket, WorldPosData, Logger, RealmHeroesLeftPacket, UsePortalPacket, MapInfoPacket } from 'nrelay';
 import { Movements } from './movements/Movements';
 import { Realm } from './data-types/Realm';
 import { Spacial } from './maths/Spacial';
@@ -12,10 +12,11 @@ import { ShotInfo } from './data-types/ShotInfo';
 class EventBotPlugin {
   private NexusWay     = new WorldPosData(107, 160.5);
   private Move         = new Movements();
-  private bestRealm    = new Realm( new WorldPosData(0, 0), -1, "init" );
+  private bestRealm    = new Realm( new WorldPosData(0, 0), -1, "init", 0 );
   private lookForRealm = false;
   private lock         = false;
   private isInNexus    = true;
+  private sendPortal   = false;
   private entities     = new Map<string, Array<ShotInfo>>();
 
   constructor() {
@@ -26,15 +27,30 @@ class EventBotPlugin {
 
   @PacketHook()
   public onNewTickPacket( client: Client, newTickPacket: NewTickPacket ): void {
-    if ( client.mapInfo.name == "Nexus" ) {
+    if ( client.mapInfo.name == "Nexus" && this.isInNexus ) {
       if( !Spacial.isOnPos(client.worldPos, new WorldPosData(107, 132)) && !this.lock ) {
         client.nextPos.push(this.NexusWay);
         client.nextPos.push( new WorldPosData(107, 132) );
         this.lock = true;
       } else {
-        if(!Spacial.isOnPos(client.worldPos, this.bestRealm.pos))
+        if(!Spacial.isOnPos(client.worldPos, this.bestRealm.pos)) {
           this.getOutOfNexus( client );
+        } else if ( Spacial.isOnPos(client.worldPos, this.bestRealm.pos) ) {
+          this.isInNexus  = false;
+          this.sendPortal = true;
+        }
       }
+    }
+  }
+
+  @PacketHook()
+  public onMapInfo( client: Client, mapInfoPacket: MapInfoPacket ) {
+    this.isInNexus = mapInfoPacket.name == "Nexus" ? true : false;
+
+    if ( this.isInNexus ) {
+      client.autoNexusThreshold = 0;
+    } else {
+      client.autoNexusThreshold = 20;
     }
   }
 
@@ -47,6 +63,11 @@ class EventBotPlugin {
         this.bestRealm = realm;
       }
     }
+
+    if ( this.sendPortal && client.nextPos.length == 0 ) {
+      this.UsePortal( client, this.bestRealm.id );
+      this.sendPortal = false;
+    }
   }
 
   private getOutOfNexus( client: Client ) : void {
@@ -56,11 +77,10 @@ class EventBotPlugin {
   }
 
   private findBestRealm( data: ObjectData[] ) : Realm {
-    var best = new Realm( new WorldPosData(0, 0), -1, "init" );
+    var best = new Realm( new WorldPosData(0, 0), -1, "init", 0 );
 
     data.forEach(function(obj) {
-      obj.status.stats.forEach(function(val){
-
+      obj.status.stats.forEach(function(val){ 
         if ( val.stringStatValue != undefined ){
 
           if ( val.stringStatValue.startsWith("NexusPortal") ){
@@ -68,12 +88,22 @@ class EventBotPlugin {
             var name    = val.stringStatValue.split(".")[1].split(" ")[0];
 
             if ( Number(players) > best.players )
-              best = new Realm( obj.status.pos, Number(players), name );
+              best = new Realm( obj.status.pos, Number(players), name, obj.status.objectId );
           }
         }
       });
     });
 
     return best;
+  }
+
+  private UsePortal( client: Client,objId: number ) {
+    Logger.log( "Event Bot", `Connected to realm ${this.bestRealm.name}` );
+
+    var usePortal       = new UsePortalPacket();
+    usePortal.objectId  = objId;
+    usePortal.propagate = true;
+
+    client.io.send( usePortal );
   }
 }
